8711554699217c6315d6291e78884ad5b0e3b174
lloydzhou <lloydzhou@qq.com>
2015-10-25 00:14:54 +0800

    update skip and limit

--- index.php
+++ index.php
@@ -27,38 +27,38 @@ class Api extends Base{
         'categories' => array_map(function($c){ return array('id'=>$c->id, 'name'=>$c->name);}, $categories) ,
     );
 })
-->get('/category/:id', function($id){
-    $word = (new Word)->eq('cid', $id)->orderby('id desc')->findAll();
+->get('/category/:id', function($id, $skip=0, $limit=10){
+    $word = (new Word)->eq('cid', $id)->orderby('id desc')->limit($skip, $limit)->findAll();
     return array(
         'word' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->name); }, $word)
     );
 })
-->get('/user/:id/comment', function($id){
-    $comments = (new Comment)->eq('uid', $id)->orderby('id desc')->findAll();
+->get('/user/:id/comment', function($id, $skip=0, $limit=10){
+    $comments = (new Comment)->eq('uid', $id)->orderby('id desc')->limit($skip, $limit)->findAll();
     return array(
         'coment' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->content); }, $comments)
     );
 })
-->get('/user/:id/desc', function($id){
-    $descs = (new Description)->eq('uid', $id)->orderby('id desc')->findAll();
+->get('/user/:id/desc', function($id, $skip=0, $limit=10){
+    $descs = (new Description)->eq('uid', $id)->orderby('id desc')->limit($skip, $limit)->findAll();
     return array(
         'desc' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->content); }, $descs)
     );
 })
-->get('/user/:id/word', function($id){
-    $word = (new Word)->eq('uid', $id)->orderby('id desc')->findAll();
+->get('/user/:id/word', function($id, $skip=0, $limit=10){
+    $word = (new Word)->eq('uid', $id)->orderby('id desc')->limit($skip, $limit)->findAll();
     return array(
         'word' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->name); }, $word)
     );
 })
-->get('/search/:name', function($name){
-    $word = (new Word)->like('name', '%'. $name. '%')->orderby('id desc')->findAll();
+->get('/search/:name', function($name, $skip=0, $limit=10){
+    $word = (new Word)->like('name', '%'. $name. '%')->orderby('id desc')->limit($skip, $limit)->findAll();
     return array(
         'word' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->name); }, $word)
     );
 })
-->get('/word/:id', function($id){
-    $desc = (new Description)->eq('wid', $id)->orderby('id desc')->findAll();
+->get('/word/:id', function($id, $skip=0, $limit=10){
+    $desc = (new Description)->eq('wid', $id)->orderby('id desc')->limit($skip, $limit)->findAll();
     return array(
         'desc' => array_map(function($w){
             return array('id' => $w->id, 'content' => $w->content, 'image' => array_map(function($i){
@@ -67,8 +67,8 @@ class Api extends Base{
         }, $desc)
     );
 })
-->get('/desc/:id', function($id){
-    $comments = (new Comment)->eq('did', $id)->orderby('id desc')->findAll();
+->get('/desc/:id', function($id, $skip=0, $limit=10){
+    $comments = (new Comment)->eq('did', $id)->orderby('id desc')->limit($skip, $limit)->findAll();
     return array(
         'comment' => array_map(function($w){ return array('id' => $w->id, 'content' => $w->content); }, $comments)
     );
