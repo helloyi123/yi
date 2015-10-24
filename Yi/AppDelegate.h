ea3edf7f2378fb9101f9d39c4b91a0353886cb44
talisk <talisk@foxmail.com>
2015-10-24 12:33:50 +0800

    init

--- Yi/AppDelegate.h
+++ Yi/AppDelegate.h
@@ -6,6 +6,20 @@
 //  Copyright © 2015年 sunkai. All rights reserved.
 //
 
+
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKConnector/ShareSDKConnector.h>
+
+//腾讯开放平台（对应QQ和QQ空间）SDK头文件
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+
+//微信SDK头文件
+#import "WXApi.h"
+
+//新浪微博SDK头文件
+#import "WeiboSDK.h"
+//新浪微博SDK需要在项目Build Settings中的Other Linker Flags添加"-ObjC"
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
