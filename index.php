e6ac2b25b5bcf33b0b64355f1948906d3c011ea0
lloydzhou <lloydzhou@qq.com>
2015-10-24 23:21:49 +0800

    update api

--- index.php
+++ index.php
@@ -27,6 +27,34 @@ class Api extends Base{
         'categories' => array_map(function($c){ return array('id'=>$c->id, 'name'=>$c->name);}, $categories) ,
     );
 })
-->get('/', function(){
+->get('/category/:id', function($id){
+    $word = (new Word)->eq('cid', $id)->orderby('id desc')->findAll();
+    return array(
+        'word' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->name); }, $word)
+    );
+})
+->get('/user/:id/comment', function($id){
+    $comments = (new Comment)->eq('uid', $id)->orderby('id desc')->findAll();
+    return array(
+        'coment' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->content); }, $comments)
+    );
+})
+->get('/user/:id/desc', function($id){
+    $descs = (new Description)->eq('uid', $id)->orderby('id desc')->findAll();
+    return array(
+        'desc' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->content); }, $descs)
+    );
+})
+->get('/user/:id/word', function($id){
+    $word = (new Word)->eq('uid', $id)->orderby('id desc')->findAll();
+    return array(
+        'word' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->name); }, $word)
+    );
+})
+->get('/search/:name', function($name){
+    $word = (new Word)->like('name', '%'. $name. '%')->orderby('id desc')->findAll();
+    return array(
+        'word' => array_map(function($w){ return array('id' => $w->id, 'name' => $w->name); }, $word)
+    );
 })
 ->execute();
